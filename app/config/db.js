c0abed0859189e9f5ec10d34e676586584454ca8
delpast1 <lamquanguit@gmail.com>
2017-08-19 18:32:24 +0700

    add database password

--- app/config/db.js
+++ app/config/db.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "pharmacydemo",
     database: "pharmacy"
 });
 
